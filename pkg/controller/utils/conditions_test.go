64175fa30096cf2eb78221977229b4adabb9ae08
katherinelc321 <klin@redhat.com>
2021-09-24 12:27:41 -0400

    test

--- pkg/controller/utils/conditions_test.go
+++ pkg/controller/utils/conditions_test.go
@@ -60,29 +60,25 @@ func TestShouldUpdateCondition(t *testing.T) {
 
 // 	testData := []struct{
 // 		name string
-// 		oldStatus corev1.ConditionStatus
-// 		newStatus corev1.ConditionStatus
+// 		Status corev1.ConditionStatus
 // 		updateConditionCheck UpdateConditionCheck
 // 		expectedReturn []awsv1alpha1.AccountCondition
 // 	}{
 // 		{
 // 			name: "test different old status and new status",
-// 			oldStatus: corev1.ConditionTrue,
-// 			newStatus: corev1.ConditionFalse,
+// 			Status: corev1.ConditionTrue,
 // 			updateConditionCheck: UpdateConditionAlways,
 // 			expectedReturn: []awsv1alpha1.AccountCondition{},
 // 		},
 // 		{
 // 			name: "test same old status and new status but always update conditions",
-// 			oldStatus: corev1.ConditionFalse,
-// 			newStatus: corev1.ConditionFalse,
+// 			Status: corev1.ConditionFalse,
 // 			updateConditionCheck: UpdateConditionAlways,
 // 			expectedReturn: []awsv1alpha1.AccountCondition{},
 // 		},
 // 		{
 // 			name: "test same old status and new status but never update conditions",
-// 			oldStatus: corev1.ConditionTrue,
-// 			newStatus: corev1.ConditionTrue,
+// 			Status: corev1.ConditionTrue,
 // 			updateConditionCheck: UpdateConditionNever,
 // 			expectedReturn: []awsv1alpha1.AccountCondition{},
 // 		},
@@ -91,13 +87,13 @@ func TestShouldUpdateCondition(t *testing.T) {
 // 	for _, test := range testData {
 // 		t.Run(test.name, func(t *testing.T){
 
-// 			oldReason := "randOldReason"
-// 			oldMessage := "random old msg"
-// 			newReason := "randNewReason"
-// 			newMessage := "random new message"
-// 			ccs := true
+// 			conditions := []awsv1alpha1.AccountCondition{}
+// 			conditionType := awsv1alpha1.AccountInitializingRegions
+// 			reason := ""
+// 			message := ""
+// 			ccs := false
 
-// 			returnVal := SetAccountCondition()
+// 			returnVal := SetAccountCondition(conditions, conditionType, test.Status, reason, message, test.updateConditionCheck, ccs)
 // 		})
 // 	}
 // }
